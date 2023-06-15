d7f50ff16acc9669214bbfc63c6271a489aeed86
modupi <modupimaphoto@gmail.com>
2023-06-15 12:28:00 +0200

    homepage

---
+++ src/App.js
@@ -0,0 +1,11 @@
+const App = () => {
+
+    return(
+        <div className='container'>
+            <h1 className='text-center'>
+                I come from <code>App.js</code>
+            </h1>
+        </div>
+    )
+}
+export default App;
