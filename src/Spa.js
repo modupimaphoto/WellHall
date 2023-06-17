9d6f72388f67642442fe0c5ce0eba703c7cbb7be
modupi <modupimaphoto@gmail.com>
2023-06-17 21:07:18 +0200

    Spa info

---
+++ src/Spa.js
@@ -0,0 +1,32 @@
+
+const Spa = () => {
+
+    return(
+        <div className='container'>
+            <div className='row'>
+                <div className='col-md-6'>
+
+                </div>
+                <div className='col-md-6'>
+                    <h5>
+                        Experience the Harmony of Body, Mind, and Spirit.
+                    </h5>
+                    <h3>
+                        Spa.
+                    </h3>
+                    <p>
+                        At Harmony Resort, we believe that wellness is not only a physical state, but also a mental and emotional one. 
+                    </p>
+                    <p>
+                        That’s why we offer a range of spa treatments that can help you relax, rejuvenate, and restore your harmony of body, mind, and spirit.
+                    </p>
+                    <p>
+                        Our spa is a world-class facility that features a sauna, a steam room, a jacuzzi, and a relaxation lounge. 
+                        Our spa also uses natural and organic products that are gentle on your skin and the environment.
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Spa;
